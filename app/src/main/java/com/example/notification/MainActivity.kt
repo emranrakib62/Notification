d87cba127dfc97f0266bc8bd92f344e0be58a99e
Emranul Hoque <emranrakib01862@gmail.com>
2025-07-26 00:51:06 +0600

    updated

--- app/src/main/java/com/example/notification/MainActivity.kt
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.notification
 
+import android.app.NotificationManager
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+lateinit var notificationManager:NotificationManager
+lateinit var Builder:NotificationCompat.Builder
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         
+    }
+    fun btnclick(v:View){
+
     }
 }
