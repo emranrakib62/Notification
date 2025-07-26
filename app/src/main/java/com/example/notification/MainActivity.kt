3a7afca9b12aab5d48dcd3c4e8450019c4e9c75d
Emranul Hoque <emranrakib01862@gmail.com>
2025-07-26 10:15:08 +0600

    updated

--- app/src/main/java/com/example/notification/MainActivity.kt
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.notification
 
 import android.app.NotificationManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -25,7 +29,14 @@ lateinit var builder:NotificationCompat.Builder
        builder=NotificationCompat.Builder(this,"n_channel")
            .setContentTitle("just a title")
            .setContentText("just a body")
+           .setLargeIcon(getmyBitmap(R.drawable.noti))
            .setSmallIcon(R.drawable.ic_stat_name)
 
+    }
+
+    private fun getmyBitmap(imgres: Int): Bitmap {
+val drawable:Drawable? =ResourcesCompat.getDrawable(resources,imgres,null)
+var drBitmap:BitmapDrawable=drawable as BitmapDrawable
+        return drBitmap.bitmap
     }
 }
