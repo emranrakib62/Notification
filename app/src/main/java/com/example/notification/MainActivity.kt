95116f040aa649a8483a509bb5cf6ba186e2f945
Emranul Hoque <emranrakib01862@gmail.com>
2025-07-27 18:24:29 +0600

    updated

--- app/src/main/java/com/example/notification/MainActivity.kt
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -10,6 +10,7 @@ import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
@@ -21,32 +22,47 @@ class MainActivity : AppCompatActivity() {
 
 lateinit var notificationManager:NotificationManager
 lateinit var builder:NotificationCompat.Builder
+lateinit var btnnotif:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         notificationManager=getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+          btnnotif=findViewById(R.id.btnnotif)
+
+        btnnotif.setOnClickListener {
+
+            val intent=Intent(this,MainActivity2::class.java)
+            intent.putExtra("key","From Notification")
+
+            val pendingIntent=PendingIntent.getActivity(applicationContext,101,intent ,PendingIntent.FLAG_UPDATE_CURRENT)
+
+            builder=NotificationCompat.Builder(this,"n_channel")
+                .setContentTitle("just a title")
+                .setContentText("just a body")
+                .setLargeIcon(getmyBitmap(R.drawable.noti))
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentIntent(pendingIntent)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationManager.createNotificationChannel(NotificationChannel("n_channel","",NotificationManager.IMPORTANCE_HIGH))
+            }
+            val id=System.currentTimeMillis().toInt()
+
+            notificationManager.notify(id,builder.build())
+
+
+
+        }
+
+
 
     }
+
+
+
+
     fun btnclick(v:View){
 
-val intent=Intent(this,MainActivity2::class.java)
-intent.putExtra("key","From Notification")
-
-        val pendingIntent=PendingIntent.getActivity(applicationContext,101,intent ,PendingIntent.FLAG_UPDATE_CURRENT)
-
-       builder=NotificationCompat.Builder(this,"n_channel")
-           .setContentTitle("just a title")
-           .setContentText("just a body")
-           .setLargeIcon(getmyBitmap(R.drawable.noti))
-           .setSmallIcon(R.drawable.ic_stat_name)
-           .setContentIntent(pendingIntent)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            notificationManager.createNotificationChannel(NotificationChannel("n_channel","",NotificationManager.IMPORTANCE_HIGH))
-        }
-val id=System.currentTimeMillis().toInt()
-
-        notificationManager.notify(id,builder.build())
 
     }
 
